2e4f9b2d661a184407ef9663f51f257871203998
SeongMin-K <eo1tks3@naver.com>
2022-01-02 16:38:35 +0900

    Initial Commit

---
+++ ScrollViewReader_SwiftUI/ScrollViewReader_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReader_SwiftUIApp.swift
+//  ScrollViewReader_SwiftUI
+//
+//  Created by SeongMinK on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReader_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
